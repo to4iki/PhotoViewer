2b00871235c9836929754a10a355564aefda7b62
tsk takezawa <tsk.take815@gmail.com>
2017-09-06 17:51:08 +0900

    Add API framework

---
+++ APITests/APITests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import API
+
+final class APITests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+    }
+}
