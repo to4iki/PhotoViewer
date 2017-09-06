b6cca31baa7053e8c5f9f2c10394bd3e538bd8d4
tsk takezawa <tsk.take815@gmail.com>
2017-09-06 17:47:33 +0900

    Initial Commit

---
+++ PhotoViewerTests/PhotoViewerTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import PhotoViewer
+
+final class PhotoViewerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+    }
+}
